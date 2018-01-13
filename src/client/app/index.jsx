03773553b6c6030ecb0a45624b44809a77ff5343
Eduardo Alvarez Castañeda <eduardo.a1993@gmail.com>
2018-01-13 20:43:19 -0300

    avance el sitio web

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  	render () {
+    	return(
+    		<p> Hello React project a</p>
+    	)
+  	};
+};
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const BUILD_DIR = path.resolve(__dirname, 'src/client');
 const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const PUBLIC_PATH = path.resolve(__dirname, '/src/client');
 
 const config = {
-  	entry: APP_DIR + '/index.jsx',
-  	output: {
-    	path: BUILD_DIR,
-    	filename: 'bundle.js'
-  	}
+	entry: APP_DIR + '/index.jsx',
+	output: {
+  	path: BUILD_DIR,
+  	filename: 'bundle.js',
+    publicPath: PUBLIC_PATH
+	},
+  devServer: {
+    host: '0.0.0.0',
+    port: 8093,
+    compress: true,
+    inline: true
+  },
+	module : {
+  	loaders : [
+ 			{
+      	test : /\.jsx?/,
+      	include : APP_DIR,
+      	loader : 'babel-loader'
+    	}
+  	]
+	}
 };
 
 module.exports = config;
