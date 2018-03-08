a26abe8c35f1d4c48974d678efc65d64c4ca0782
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-03-07 23:35:27 -0300

    Crear el header component

---
+++ src/client/app/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Eduardo Alvarez Castañeda</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import Header from './Header/';
+
 class App extends Component {
-  	render () {
-    	return(
-    		<p> Hello React project a</p>
-    	)
-  	};
+  render () {
+  	return(
+  		<div className="container">
+          <Header />
+      </div>
+  	);
+  };
 };
 
 render(<App />, document.getElementById('app'));
