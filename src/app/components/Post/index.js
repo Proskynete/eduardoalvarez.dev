122c9cf9af7334559554f956783360f450f672b0
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-03-20 23:59:12 -0300

    Terminar de editar css de posts

--- src/app/components/Post/index.js
+++ src/app/components/Post/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 import './Post.scss';
 
@@ -10,17 +12,22 @@ class Post extends Component {
 
   render() {
     return (
-      <section className="post col-12 col-md-8">
+      <section className="post col-12 col-md-7">
         <div className="post__header">
-          <h2 className="post__header__author-name">Eduardo Alvarez</h2>
-          <h4 className="post__header__date">{this.props.content.createDate}</h4>
-          <h1 className="post__header__title">{this.props.content.title}</h1>
+          <h2 className="post__header__author-name">{this.props.content.authorName}</h2>
+          <h4 className="post__header__date">
+            <Moment format="LL" locale="es">
+              {this.props.content.createDate}
+            </Moment>
+          </h4>
         </div>
         <div className="post__body">
+          <h1 className="post__body__title">{this.props.content.title}</h1>
           <p className="post__body__content">{this.props.content.shortDescription}</p>
         </div>
         <div className="post__footer text-right">
-          <h3 className="post__footer__category">{this.props.content.category}</h3>
+          <p className="post__footer__title">Categorías</p>
+          <h3 className="post__footer__categories">{this.props.content.category}</h3>
         </div>
       </section>
     );
