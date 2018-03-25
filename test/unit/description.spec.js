0f5fbe4d49f3c966e4a5adcd105868313badf4b0
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-03-25 01:13:29 -0300

    Agregar dependencias para test

---
+++ test/unit/description.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import Description from '../../src/app/views/Description/';
+import config from '../../src/config/config';
+
+describe('<Description />', () => {
+  it('should render the component', () => {
+    const wrapper = shallow(<Description />);
+    expect(wrapper.find('.description__logo')).to.be.equal(config.description.logoName);
+  });
+});
