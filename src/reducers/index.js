1e12cbc43973a84075707f82e5e998f950bdcc84
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-04-07 09:50:53 -0300

    Agregar logica para mostrar lista de posts o el detalle de la misma

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import showPosts from './posts';
+import showDescription from './description';
 
 const rootReducer = combineReducers({
   posts: showPosts,
+  profile: showDescription,
 });
 
 export default rootReducer;
