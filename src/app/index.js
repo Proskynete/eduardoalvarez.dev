ac663a513db55c7b02ea95074229bc5a42845f20
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-04-15 04:31:55 -0300

    limpiar archivos innecesarios

--- src/app/index.js
+++ src/app/index.js
@@ -1,39 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { showDescription } from '../actions/show_description';
+import React from 'react';
 
 import './index.scss';
 
 import Description from './views/Description/';
 import Posts from './views/Posts/';
-import ContentPost from './views/ContentPost/';
 
+const App = () =>
+  (<div className="row">
+    <div className="col-12 col-md-3">
+      <Description />
+    </div>
+    <div className="col-12 col-md-9">
+      <Posts />
+    </div>
+  </div>);
 
-class App extends Component {
-  componentWillMount() {
-    this.props.showDescription();
-  }
-
-  render() {
-    return (<div className="row">
-      <div className="col-12 col-md-3">
-        <Description />
-      </div>
-      <div className="col-12 col-md-9">
-        {this.props.profile === 'listPosts' ? <Posts /> : <ContentPost /> }
-      </div>
-    </div>);
-  }
-}
-
-const mapStateToProps = state => ({
-  profile: state.profile.profile,
-});
-
-App.propTypes = {
-  showDescription: PropTypes.func.isRequired,
-  profile: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, { showDescription })(App);
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import showPosts from './posts';
-import showDescription from './description';
 
 const rootReducer = combineReducers({
   posts: showPosts,
-  profile: showDescription,
 });
 
 export default rootReducer;
