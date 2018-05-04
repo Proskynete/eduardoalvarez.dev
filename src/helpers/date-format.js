8949907364e4102fbdfa1557824a1f20ef3bb90a
Eduardo Alvarez <eduardo.alvarez@globant.com>
2018-05-04 19:35:50 -0300

    Eliminar react-moment y hacer un nuevo helper para formateo de fechas

---
+++ src/helpers/date-format.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+
+const onlyDate = datetime => datetime.split('T')[0];
+const onlyTime = datetime => datetime.split('T')[1].split('.')[0];
+const agoFormat = datetime => moment(datetime).locale('es').fromNow();
+const prettyFormat = datetime => moment(datetime).locale('es').format('LL');
+
+
+export {
+  onlyDate,
+  onlyTime,
+  agoFormat,
+  prettyFormat,
+};
