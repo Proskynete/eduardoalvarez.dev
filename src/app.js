0166558fdfbb92a3c50a2aa6edea425f33fe7d44
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-03-29 21:42:49 -0300

    Agregar react-router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import Nav from './components/nav/';
 
 const App = () => (
-    <Nav />
+    <BrowserRouter>
+        <Nav />
+
+        <Route path="/" exact component={Home} />
+        <Route path="/quien-soy/" component={WhoIAm} />
+        <Route path="/now/" component={Now} />
+        <Route path="/blog/" component={Blog} />
+        <Route path="/hablemos/" component={LetsTalk} />
+    </BrowserRouter>
 );
 
 export default App;
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './index.scss';
 
-const Nav = () => <nav className="nav">
-    <li>Home</li>
-    <li>Quien Soy</li>
-    <li>Now</li>
-    <li>Blog</li>
-    <li>Hablemos</li>
-</nav>
+const Nav = () => (
+    <nav className="nav">
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/quien-soy/">Quien soy</Link>
+        </li>
+        <li>
+            <Link to="/now/">Now</Link>
+        </li>
+        <li>
+            <Link to="/blog/">Blog</Link>
+        </li>
+        <li>
+            <Link to="/hablemos/">Hablemos</Link>
+        </li>
+    </nav>
+);
 
 export default Nav;
