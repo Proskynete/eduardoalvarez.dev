d0f16318f870c1d7eddbf157aeb18eebc64a1a1e
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-04-02 15:04:40 -0300

    Crear header component

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.scss';
+
+const Header = () => (
+    <div className="header">
+        <div className="header__inner">
+            <div className="header__inner__container">
+                <div className="header__inner__container__logo">
+                    EDUARDOALVAREZ
+                </div>
+                <div className="header__inner__container__slogan">
+                    Si no estás dispuesto a darlo todo, no tienes derecho a intentarlo
+                </div>
+                <div className="header__inner__container__socials">
+                    Linkedin - Github - Curriculum
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Header;
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../../components/header/';
 
 const Home = () => (
     <div className="container-fluid">
         <div className="row justify-content-md-center">
+            <div className="col-12">
+                <Header />
+            </div>
             <div className="col col-md-8">
                 <h1>Home</h1>
             </div>
