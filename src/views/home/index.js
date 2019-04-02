5af16b53427d75fb598fe0fedfd147be9425129d
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-04-02 18:32:13 -0300

    Agregar contenido al home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/header/';
+import './index.scss';
 
 const Home = () => (
     <div className="container-fluid">
@@ -8,7 +9,14 @@ const Home = () => (
                 <Header />
             </div>
             <div className="col col-md-8">
-                <h1>Home</h1>
+                <div className="container">
+                    <h1 className="container__title">Quien soy en 10 segundos</h1>
+                    <p className="container__content">
+                        Soy estudiante de ingeniería en informática en la <a className="container__content__link" href="https://www.unab.cl/es/">UNAB</a>,
+                        desarrollador web (Web UI) en la empresa <a className="container__content__link" href="http://www.globant.com">Globant</a>.
+                        Busco poder compartir mi conocimiento en este blog
+                    </p>
+                </div>
             </div>
         </div>
     </div>
