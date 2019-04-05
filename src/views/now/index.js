555eec15fe4167a8da4f6ff2fe324f81f8d4b57c
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-04-05 19:10:46 -0300

    Agregar contenido en now

--- src/views/now/index.js
+++ src/views/now/index.js
@@ -16,28 +16,44 @@ const Now = () => (
                 <div className="container">
                     <h1 className="container__title">Now</h1>
                     <p className="container__content">
-                        Estoy viviendo en el centro de Santigo.
+                        <span className="container__content__question">Donde vivo?</span>
+                        Santiago de Chile. Santiago centro.
                     </p>
                     <p className="container__content">
+                        <span className="container__content__question">Cuantos años tengo?</span>
                         Tengo 25 años.
                     </p>
                     <p className="container__content">
+                        <span className="container__content__question">Estudiando</span>
                         Me encuentro estudiando Ingeniería en Computación e Informática en la
                         universidad <LinkWeb link="https://www.unab.cl/" title="Andres Bello (UNAB)" />.
                     </p>
                     <p className="container__content">
+                        <span className="container__content__question">Donde trabajo?</span>
                         Soy Web UI SSr en la empresa <LinkWeb link="http://www.globant.com" title="Globant" />.
                     </p>
                     <p className="container__content">
+                        <span className="container__content__question">Que juego estoy jugando?</span>
                         Silver III en el juego <LinkWeb link="http://www.leagueoflegends.com" title="League of legends" />, servidor de LAS. Nombre
                         de invocador <span className="container__content__destaked">Proskynete</span>.
                     </p>
                     <p className="container__content">
+                        <span className="container__content__question">Algo que haya realizado hace poco y que me llene de felicidad</span>
                         Dicté mi primera charla sobre las tecnologías <span className="container__content__destaked">React/Redux</span> para las chicas
-                        de <LinkWeb link="http://www.laboratoria.com" title="Laboratoria Chile" />
+                        de <LinkWeb link="http://www.laboratoria.com" title="Laboratoria Chile" />.
                     </p>
                     <p className="container__content">
-                        Estoy estudiando React-Native
+                        <span className="container__content__question">Alguna nueva tecnología?</span>
+                        Estoy estudiando React-Native.
+                    </p>
+                    <p className="container__content">
+                        <span className="container__content__question">Que estoy leyendo?</span>
+                        Estoy leyendo <span className="container__content__destaked">HTML 5 - Notes for professionals</span>.
+                    </p>
+                    <p className="container__content">
+                        <span className="container__content__question">Que es lo que estoy desarrollando?</span>
+                        Actualmente me encuentro generando contenido para la
+                        sección <span className="container__content__destaked">Blog</span> de mi sitio web.
                     </p>
                 </div>
                 <Line />
