67a635bddd2dadf2ba7b44dafee28c25fbc39977
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-04-05 22:28:54 -0300

    Cambiar estilos en nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom";
 import './index.scss';
 
 const Nav = () => (
-    <nav className="nav">
-        <li className="nav__item">
-            <Link className="nav__item__link" to="/">Home</Link>
-        </li>
-        <li className="nav__item">
-            <Link className="nav__item__link" to="/quien-soy">Quien soy</Link>
-        </li>
-        <li className="nav__item">
-            <Link className="nav__item__link" to="/now">Now</Link>
-        </li>
-        <li className="nav__item">
-            <Link className="nav__item__link" to="/blog">Blog</Link>
-        </li>
-    </nav>
+    <div className="nav">
+        <nav className="nav__content">
+            <li className="nav__content__item">
+                <Link className="nav__content__item__link" to="/">Home</Link>
+            </li>
+            <li className="nav__content__item">
+                <Link className="nav__content__item__link" to="/quien-soy">Quien soy</Link>
+            </li>
+            <li className="nav__content__item">
+                <Link className="nav__content__item__link" to="/now">Now</Link>
+            </li>
+            <li className="nav__content__item">
+                <Link className="nav__content__item__link" to="/blog">Blog</Link>
+            </li>
+        </nav>
+    </div>
 );
 
 export default Nav;
