c11fb3174dcf09228054ce66efcaf2b59d1d82d0
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-04-06 10:50:05 -0400

    add new url to prod environment

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export default {
 		const url = {
 			development: 'http://localhost:8882',
 			integration: 'http://localhost:3000',
-			production: 'production_url',
+			production: 'http://50.116.19.13:3000',
 		};
 		return url[this.handleGetEnvironment()];
 	},
