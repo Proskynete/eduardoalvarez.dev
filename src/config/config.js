eef937e48d9687425cda5dfae646c5cf8e2dcaf4
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-04-06 13:41:33 -0400

    Change url to prod

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export default {
 		const url = {
 			development: 'http://localhost:8882',
 			integration: 'http://localhost:3000',
-			production: 'http://50.116.19.13:3000',
+			production: 'https://api.eduardoalvarez.cl',
 		};
 		return url[this.handleGetEnvironment()];
 	},
