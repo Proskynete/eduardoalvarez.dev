6aaa6d4d967aed8b9dd01fb6154abcae2157aa97
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-04-30 16:56:41 -0400

    Refactor

--- src/components/highlighted/index.js
+++ src/components/highlighted/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.scss';
 
 const Highlighted = props => {
 	const { content } = props;
 
-	return <span className="container__content__highlighted">{content}</span>;
+	return <span className="highlighted">{content}</span>;
 };
 
 Highlighted.propTypes = {
