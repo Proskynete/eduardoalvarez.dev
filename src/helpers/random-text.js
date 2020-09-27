7d6f313a5f1cf18657ac2325a43c0eddb8115a03
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-09-27 20:29:40 -0300

    Init with nextjs

--- src/helpers/random-text.js
+++
@@ -1,6 +0,0 @@
-const randomText = (strings) => {
-	const string = strings.map((_string) => _string.text);
-	return string[Math.floor(Math.random() * strings.length)];
-};
-
-export default randomText;
