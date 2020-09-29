52509b1e16d8f9015494089d0dbbf6e1fbd703f7
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-09-28 22:19:01 -0300

    First api example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import { FC } from "react";
 import Head from "next/head";
+import useSWR from "swr";
 
-const Home: FC = () => {
+const fetcher = async (url: string): Promise<string> => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (res.status !== 200) throw new Error(data.message);
+
+  return data;
+};
+
+const Home: FC = (props) => {
+  console.log(props);
+  const { data, error } = useSWR("/api/hello", fetcher);
+
+  if (error) return <p>Error in server</p>;
+  if (!data) return <p>Cargando...</p>;
+
   return (
     <>
       <Head>
