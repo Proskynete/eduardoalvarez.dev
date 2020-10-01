f6bb361b9c2a9581a4a520af9a5512d39f9b2d43
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-09-30 23:24:35 -0300

    Add format to dates

--- helpers/date.helper.tsx
+++ helpers/date.helper.tsx
@@ -1,7 +1,23 @@
 import moment from "moment";
 moment.locale("es");
 
-export const onlyDate = (datetime: string): string => datetime.split("T")[0];
+const getDiffDates = (datetime: string): number => {
+  const today = moment(new Date());
+  const date = moment(datetime);
+  return today.diff(date, "days");
+};
 
-export const prettyFormat = (datetime: string): string =>
+const dateFromNow = (datetime: string): string => moment(datetime).fromNow();
+
+const dateFormated = (datetime: string): string =>
   moment(datetime).format("LL");
+
+export const prettyFormat = (datetime: string): string => {
+  const _diff = getDiffDates(datetime);
+
+  if (_diff <= 7) {
+    return dateFromNow(datetime);
+  }
+
+  return dateFormated(datetime);
+};
