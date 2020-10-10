559b9cfaac2f5e53811af7b2cc1a720a9cdc9453
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-10-09 21:42:11 -0300

    Try subscribed a new user

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -11,6 +11,8 @@ interface SuccessResponseInterface {
 
 export default async (req: NextApiRequest, res: NextApiResponse<SuccessResponseInterface | ErrorResponseInterface>) => {
   const { email, name } = req.body;
+  console.log('req.body', email, name);
+
   const url = ({ _datacenter, _listid }) => `https://${_datacenter}.api.mailchimp.com/3.0/lists/${_listid}/members`;
 
   if (!email || !name) return res.status(409).json({ error: "Los datos son requeridos" });
@@ -20,11 +22,16 @@ export default async (req: NextApiRequest, res: NextApiResponse<SuccessResponseI
     const API_KEY = process.env.MAILCHIMP_API_KEY;
     const DATACENTER = API_KEY.split("-")[1];
 
+    console.log("ENV VARIABLES");
+    console.log(LIST_ID, API_KEY, DATACENTER);
+
     const data = {
       email_addres: email,
       first_name: name,
-      status: "subscribed"
+      status: 'subscribed'
     }
+
+    console.log(data)
 
     const response = await fetch(url({ _datacenter: DATACENTER, _listid: LIST_ID }), {
       body: JSON.stringify(data),
@@ -33,6 +40,9 @@ export default async (req: NextApiRequest, res: NextApiResponse<SuccessResponseI
         'Content-Type': "application/json"
       }, method: "POST"
     });
+
+    console.log("response");
+    console.log(response);
 
     if (response.status >= 400) return res.status(410).json({ error: "Hubo un error al intentar registrar el correo, no obstante puedes mandarme un correo a [soy@eduardoalvarez.cl] y así te agrego manualmente." });
 
