ed9e41bd871735585bcc6032cf85120b22dc843d
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-10-18 19:16:54 -0300

    Required dotenv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/globals.scss';
+require('dotenv').config();
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps } from 'next/app';
