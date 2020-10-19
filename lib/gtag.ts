93244ac5ccc5a51346a5b559dfb9e9f74f8c878e
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-10-19 14:19:20 -0300

    Add google tag manager script

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,5 @@
 export const GA_TRACKING_ID = 'UA-180766853-1';
+export const GTM_TRACKING_ID = 'GTM-WSK62BX';
 
 export const pageview = (url: string) => {
 	window.gtag('config', GA_TRACKING_ID, {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import { GA_TRACKING_ID } from 'lib/gtag';
+/* eslint-disable jsx-a11y/iframe-has-title */
+import { GA_TRACKING_ID, GTM_TRACKING_ID } from 'lib/gtag';
 import Document, {
 	DocumentContext,
 	Head,
@@ -35,8 +36,31 @@ class MyDocument extends Document {
           `,
 						}}
 					/>
+
+					{/* Google Tag Manager */}
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','${GTM_TRACKING_ID}');`,
+						}}
+					/>
+					{/* End Google Tag Manager */}
 				</Head>
 				<body>
+					{/* Google Tag Manager (noscript) */}
+					<noscript>
+						<iframe
+							src={`https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}`}
+							height='0'
+							width='0'
+							style={{ display: 'none', visibility: 'hidden' }}
+						></iframe>
+					</noscript>
+					{/* End Google Tag Manager (noscript) */}
 					<Main />
 					<NextScript />
 				</body>
