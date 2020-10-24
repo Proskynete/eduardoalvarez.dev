9a6d17b1b701631a17dcdf984ebd7b5982387ca0
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-10-24 11:17:02 -0300

    Fix minor errors

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,15 +1,17 @@
 export const GA_TRACKING_ID = 'UA-180766853-1';
 
 export const pageview = (url: string) => {
-	window.gtag('config', GA_TRACKING_ID, {
-		page_path: url,
-	});
+	window.gtag &&
+		window.gtag('config', GA_TRACKING_ID, {
+			page_path: url,
+		});
 };
 
 export const event = ({ action, category, label, value }) => {
-	window.gtag('event', action, {
-		event_category: category,
-		event_label: label,
-		value: value,
-	});
+	window.gtag &&
+		window.gtag('event', action, {
+			event_category: category,
+			event_label: label,
+			value: value,
+		});
 };
