5d119c49a2c80c4bf683aafa2c6bc7964e07319f
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-11-15 20:38:55 -0300

    Minor changes

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -57,7 +57,7 @@ const prettier = require('prettier');
 
 		const formatted = prettier.format(rss, {
 			...prettierConfig,
-			parser: 'html',
+			parser: 'babel',
 		});
 
 		fs.writeFileSync('public/rss.xml', formatted);
