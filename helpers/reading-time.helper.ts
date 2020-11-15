258aca6c8b87b31fb9400686999257e839e27ffb
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-11-15 13:32:10 -0300

    Minor changes in prettyReadingTime function

--- helpers/reading-time.helper.ts
+++ helpers/reading-time.helper.ts
@@ -3,10 +3,10 @@ export const prettyReadingTime = (reading_time: number): string => {
 
 	if (reading_time < 1) {
 		textResponse = 'Menos de un minuto';
-	} else if (reading_time === 1 || Math.ceil(reading_time) === 1) {
+	} else if (reading_time === 1) {
 		textResponse = 'Lectura de 1 minuto';
 	} else {
-		textResponse = `Lectura de ${Math.ceil(reading_time)} minutos`;
+		textResponse = `Lectura de ${reading_time} minutos`;
 	}
 
 	return textResponse;
