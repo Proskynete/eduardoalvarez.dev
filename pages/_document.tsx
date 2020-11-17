c66212536af718b3477bf6c26afc7e8aa53e01f5
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-11-17 15:00:22 -0300

    Modify noscript tgm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -66,14 +66,15 @@ class MyDocument extends Document<DocumentInterface> {
 				</Head>
 				<body>
 					{isProduction && (
-						<>
 							{/* Google Tag Manager (noscript) */}
-							<noscript
-								dangerouslySetInnerHTML={{
-									__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-								}}
-							/>
-						</>
+							<noscript>
+								<iframe
+									src={`https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}`}
+									height='0'
+									width='0'
+									style={{ display: 'none', visibility: 'hidden' }}
+								></iframe>
+							</noscript>
 					)}
 					<Main />
 					<NextScript />
