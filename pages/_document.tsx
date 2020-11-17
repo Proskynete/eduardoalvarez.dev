efdbbc28f76ac7e033d2e0989cba4311656b5c98
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-11-17 15:28:49 -0300

    Modify ids for GTM and GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,7 +44,6 @@ class MyDocument extends Document<DocumentInterface> {
 										window.dataLayer = window.dataLayer || [];
 										function gtag(){dataLayer.push(arguments);}
 										gtag('js', new Date());
-
 										gtag('config', '${GA_TRACKING_ID}');
 									`,
 								}}
@@ -53,8 +52,7 @@ class MyDocument extends Document<DocumentInterface> {
 							{/* Google Tag Manager */}
 							<script
 								dangerouslySetInnerHTML={{
-									__html: `
-									(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+									__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
