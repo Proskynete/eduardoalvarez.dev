e8522c90cd60234d26e94e9c209c428d7bff5c5a
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-11-17 14:48:39 -0300

    Fix errors in production

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,10 +44,8 @@ class MyDocument extends Document<DocumentInterface> {
 										window.dataLayer = window.dataLayer || [];
 										function gtag(){dataLayer.push(arguments);}
 										gtag('js', new Date());
-										
-										gtag('config', '${GA_TRACKING_ID}', {
-											page_path: window.location.pathname,
-										});
+
+										gtag('config', '${GA_TRACKING_ID}');
 									`,
 								}}
 							/>
@@ -57,9 +55,9 @@ class MyDocument extends Document<DocumentInterface> {
 								dangerouslySetInnerHTML={{
 									__html: `
 									(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-										new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-										j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-										'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 									})(window,document,'script','dataLayer','${GTM_TRACKING_ID}');`,
 								}}
 							/>
@@ -75,10 +73,8 @@ class MyDocument extends Document<DocumentInterface> {
 									__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
 								}}
 							/>
-							{/* End Google Tag Manager (noscript) */}
 						</>
 					)}
-
 					<Main />
 					<NextScript />
 				</body>
