0df63f280ef0e5187437a600cdb9149bb76d8915
Eduardo Esteban Alvarez Castañeda <eduardo.a1993@gmail.com>
2021-10-17 02:06:23 -0300

    add: Empty button to search component

--- components/SearchComponent.tsx
+++ components/SearchComponent.tsx
@@ -1,11 +1,17 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Autocomplete from "downshift";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { connectAutoComplete, Highlight } from "react-instantsearch-dom";
 
 const SearchComponent = ({ refine, hits }) => {
   const router = useRouter();
+  const [query, setQuery] = useState("");
+
+  const handleClearSearch = () => {
+    setQuery("");
+  };
 
   return (
     <Autocomplete
@@ -28,13 +34,25 @@ const SearchComponent = ({ refine, hits }) => {
               placeholder="Buscar..."
               {...getInputProps({
                 onChange(e) {
+                  setQuery(e.target.value);
                   refine(e.target.value);
                 },
               })}
+              value={query}
             />
+
+            {query.length > 0 && (
+              <div className="search__input_container__icon_container">
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className="search__input_container__icon_container__times"
+                  onClick={handleClearSearch}
+                />
+              </div>
+            )}
           </div>
 
-          {isOpen && (
+          {query.length > 0 && (
             <div className="search__hits">
               {hits.slice(0, 5).map((item: any, index: number) => (
                 <div
