9422c3c0a53e872e5a60b1f7199b8eaec23d20d6
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-12-25 16:30:26 -0300

    feat: config project with mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://eduardoalvarez.dev",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    sitemap(),
-  ],
+  integrations: [mdx(), react(), tailwind(), sitemap()],
 });
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,10 +1,18 @@
-export interface Post {
+export interface Article {
   title: string;
+  slug: string;
   description: string;
-  publishedAt: string;
-  category: string;
-  body: string;
-  slug: {
-    current: string;
-  };
+  date: string;
+  tags: string[];
+  with_introduction: boolean;
+  hero_image: string;
+  sections: string[];
+}
+
+export interface ArticleLayout {
+  file: string;
+  url: string;
+  content: Article;
+  frontmatter: Article;
+  heading: any[];
 }
