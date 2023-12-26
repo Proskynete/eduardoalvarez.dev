91fae4226d35f07a0730eb484a2dd78c1d0c92bd
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-12-26 18:23:48 -0300

    chore: modify webmanifest configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import { publishAlgoliaRSS } from "./src/scripts/algolia.ts";
 
 import config from "./src/settings";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://eduardoalvarez.dev",
   markdown: {
@@ -26,12 +26,55 @@ export default defineConfig({
     webmanifest({
       name: `Blog de ${config.author.name}`,
       short_name: `Blog de ${config.author.name}`,
-      icon: "./public/images/me/logo.webp",
+      icon: "./public/images/manifest/512x512.png",
+      icons: [
+        {
+          src: "./public/images/manifest/72x72.png",
+          type: "image/png",
+          sizes: "72x72",
+        },
+        {
+          src: "./public/images/manifest/96x96.png",
+          type: "image/png",
+          sizes: "96x96",
+        },
+        {
+          src: "./public/images/manifest/128x128.png",
+          type: "image/png",
+          sizes: "128x128",
+        },
+        {
+          src: "./public/images/manifest/144x144.png",
+          type: "image/png",
+          sizes: "144x144",
+        },
+        {
+          src: "./public/images/manifest/152x152.png",
+          type: "image/png",
+          sizes: "152x152",
+        },
+        {
+          src: "./public/images/manifest/192x192.png",
+          type: "image/png",
+          sizes: "192x192",
+        },
+        {
+          src: "./public/images/manifest/384x384.png",
+          type: "image/png",
+          sizes: "384x384",
+        },
+        {
+          src: "./public/images/manifest/512x512.png",
+          type: "image/png",
+          sizes: "512x512",
+        },
+      ],
       description: config.description,
       start_url: "/",
-      theme_color: "#030712",
-      background_color: "#030712",
+      background_color: "#FFFFFF",
+      theme_color: "#0A3F66",
       display: "standalone",
     }),
+    publishAlgoliaRSS({}),
   ],
 });
