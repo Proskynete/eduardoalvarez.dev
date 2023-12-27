05db3d566536348e3a2d59699080576fa1c3fd06
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-12-27 00:04:11 -0300

    feat: add styles to subscribe component

--- src/components/subscribe/index.tsx
+++ src/components/subscribe/index.tsx
@@ -19,33 +19,57 @@ const Subscribe = () => {
   };
 
   return (
-    <div className="">
-      <h3>Suscríbete</h3>
+    <div>
+      <div className="pb-1 ">
+        <p className="text-lg font-semibold text-gray-800 dark:text-gray-100">
+          Suscríbete a mi newsletter
+        </p>
+        <p className="text-gray-600 dark:text-gray-400">
+          Para recibir actualizaciones sobre nuevos artículos y otros
+          contenidos.
+        </p>
+      </div>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row mt-2">
         <label>
+          <span className="sr-only">Tu nombre</span>
           <input
             id="name"
             name="name"
             type="text"
             placeholder="Nombre"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="focus:ring-primary-600 w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 dark:bg-black"
           />
         </label>
 
-        <label>
+        <label className="mt-2 sm:mt-0 sm:ml-3">
+          <span className="sr-only">Tu correo electrónico</span>
           <input
             id="email"
             name="email"
             type="text"
             placeholder="Correo electrónico"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="focus:ring-primary-600 w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 dark:bg-black"
           />
         </label>
 
-        <button>Subscribirse</button>
-        {response.message && <p>{response.message}</p>}
+        <div className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3">
+          <button className="bg-primary-500 w-full rounded-md py-2 px-4 font-medium text-white sm:py-0 hover:bg-primary-700 dark:hover:bg-primary-400 focus:ring-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-black">
+            Subscribirse
+          </button>
+        </div>
       </form>
+      {response.message && (
+        <p
+          className={`mt-2 text-sm ${
+            response.status === 200
+              ? "text-green-500 dark:text-green-400"
+              : "text-red-500 dark:text-red-400"
+          }`}
+        >
+          {response.message}
+        </p>
+      )}
     </div>
   );
 };
