624aa981c4596134b6d5584bed981573bf3fd5fa
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-12-27 01:35:21 -0300

    feat: create scrolling progress bar

---
+++ src/components/scrolling-progress-bar/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const ScrollingProgressBar = () => {
+  const [state, setState] = useState<number>(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrolled = (winScroll / height) * 100;
+
+      setState(scrolled);
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <div
+      className="top-0 sticky z-50 h-[5px] bg-teal-700 transition-all duration-300"
+      style={{ width: `${state}%` }}
+    />
+  );
+};
+
+export default ScrollingProgressBar;
