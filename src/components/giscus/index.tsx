be7bc9eb635edbdae232dfe39e441f91b9847f51
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-12-28 00:08:22 -0300

    feat: create giscus wrapper component

---
+++ src/components/giscus/index.tsx
@@ -0,0 +1,27 @@
+import Giscus from "@giscus/react";
+
+interface GiscusProps {
+  slug: string;
+}
+
+const GiscusWrapper = ({ slug }: GiscusProps) => {
+  return (
+    <Giscus
+      id="comments"
+      repo="proskynete/website"
+      repoId="R_kgDOJ_yh4w"
+      category="Blog Comments"
+      categoryId="DIC_kwDOJ_yh484CcCn6"
+      mapping="specific"
+      term={`blog/${slug}`}
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="preferred_color_scheme"
+      lang="es"
+      loading="lazy"
+    />
+  );
+};
+
+export { GiscusWrapper };
