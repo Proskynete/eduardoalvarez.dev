41b9b18c8a1f3845aa12ba2893db24681bb4484b
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-03-10 23:35:29 -0300

    feat: add new view - recursos

--- src/settings/projects.ts
+++ src/settings/projects.ts
@@ -4,51 +4,26 @@ export default {
       title: "Color Contrast Checker",
       link: "https://c3.eduardoalvarez.dev",
       githubLink: "https://github.com/Proskynete/color-contrast-checker",
-      description:
-        "Calculate the contrast ratio of text and background colors.",
-      techStack: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+      description: "Calculate the contrast ratio of text and background colors.",
+      techStack: ["React", "TypeScript", "Tailwind CSS", "Next.js", "Vercel"],
     },
     {
       title: "Vertical Timeline Component React",
-      githubLink:
-        "https://github.com/Proskynete/vertical-timeline-component-react",
-      description:
-        "A simple component to generate a responsive vertical timeline",
-      techStack: [
-        "React",
-        "TypeScript",
-        "Styled Components",
-        "Coveralls",
-        "Jest",
-      ],
+      githubLink: "https://github.com/Proskynete/vertical-timeline-component-react",
+      description: "A simple component to generate a responsive vertical timeline",
+      techStack: ["React", "TypeScript", "Styled Components", "Coveralls", "Jest"],
     },
     {
       title: "Pretty Rating React",
       githubLink: "https://github.com/Proskynete/pretty-rating-react",
-      description:
-        "A small and simple library that transform you rating in icons for you web",
-      techStack: [
-        "React",
-        "TypeScript",
-        "Styled Components",
-        "Coveralls",
-        "Jest",
-      ],
+      description: "A small and simple library that transform you rating in icons for you web",
+      techStack: ["React", "TypeScript", "Styled Components", "Coveralls", "Jest"],
     },
     {
       title: "Node API Skeleton",
       githubLink: "https://github.com/Proskynete/node-api-skeleton",
-      description:
-        "Starter project for a Node API with TS, testing and CI/CD setup.",
-      techStack: [
-        "Express",
-        "TypeScript",
-        "Jest",
-        "SuperTest",
-        "Swagger",
-        "Docker",
-        "Github Actions",
-      ],
+      description: "Starter project for a Node API with TS, testing and CI/CD setup.",
+      techStack: ["Express", "TypeScript", "Jest", "SuperTest", "Swagger", "Docker", "Github Actions"],
     },
   ],
 };
