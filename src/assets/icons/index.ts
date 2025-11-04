ac54beecd31a672318652e93dd7a8fdd83edc362
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2025-11-03 21:30:12 -0300

    feat: modify icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,13 +1,17 @@
 import { ArrowLeftIcon } from "./arrow-left";
+import { CloseIcon } from "./close";
 import { GithubIcon } from "./github";
 import { MailIcon } from "./mail";
 import { MenuIcon } from "./menu";
 import { ResourcesIcon } from "./resources";
+import { SearchIcon } from "./search";
 
 export const Icon = {
   ArrowLeft: ArrowLeftIcon,
+  Close: CloseIcon,
   Github: GithubIcon,
   Mail: MailIcon,
   Menu: MenuIcon,
   Resources: ResourcesIcon,
+  Search: SearchIcon,
 };
